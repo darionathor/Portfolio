ce0b2db70448f03374d629053c724955a2bd904b
Sasa Lalic <sasalali@gmail.com>
2019-09-17 20:04:53 +0200

    Updates

---
+++ precache-manifest.d920f742d63b355162ee53d07ffe2c73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbeb278c4d9fab716fa2a94f29f7164",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5001738e8f95c990a4d4",
+    "url": "/Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bb77154d1d3d44662cf7",
+    "url": "/Portfolio/static/js/2.a648b7c1.chunk.js"
+  },
+  {
+    "revision": "5001738e8f95c990a4d4",
+    "url": "/Portfolio/static/js/main.c0df8f0a.chunk.js"
+  },
+  {
+    "revision": "111b63828b0a0a146220",
+    "url": "/Portfolio/static/js/runtime~main.1038b714.js"
+  }
+]);
